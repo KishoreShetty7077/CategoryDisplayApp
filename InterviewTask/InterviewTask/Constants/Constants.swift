e31315e174b4f0fdf5436794e91eff82354a02bb
KishoreShetty7077 <102850875+KishoreShetty7077@users.noreply.github.com>
2024-09-09 10:56:22 +0530

    Update Constants.swift

--- InterviewTask/InterviewTask/Constants/Constants.swift
+++ InterviewTask/InterviewTask/Constants/Constants.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
+// MARK: - Constants
 struct Constants {
-    
+        // MARK: - Table View Cell Identifiers
     struct TableViewCell {
         static let itemTableViewCell = "ItemTableViewCell"
         static let headerSearchTableViewCell = "HeaderSearchTableViewCell"
     }
-    
+        // MARK: - Collection View Cell Identifiers
     struct CollectionViewCell {
         static let categoryCollectioViewCell = "CategoryCollectioViewCell"
     }
